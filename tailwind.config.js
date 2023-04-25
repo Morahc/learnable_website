b50ecb2c7c9dfadf2095b0f1e13cebff7a0d4219
Morah Stephen <Morahstephen872@gmail.com>
2023-04-25 20:36:39 +0100

    style setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+      backgroundColor: {
+        'primary': '#015376',
+        'secondary':'#0380B699',
+        'dark':'#0B0D26'
+      }
+    },
   },
   plugins: [],
 }
