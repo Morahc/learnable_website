dd399162ce2bd58b434e875435124451d153cd49
Morah Stephen <Morahstephen872@gmail.com>
2023-04-27 16:08:32 +0100

    Revert "Project feature"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,7 @@
-// import About from './components/About';
-import Partner from './components/Partner';
-import Projects from './components/Projects';
-// import Contact from './components/Contact';
-
 const index = () => {
   return (
-    <>
-      {/* <About /> */}
-      <Partner/>
-      <Projects/>
-      {/* <Contact/> */}
-    </>
-  );
-};
+    <h1>HOME</h1>
+  )
+}
 
-export default index;
+export default index
