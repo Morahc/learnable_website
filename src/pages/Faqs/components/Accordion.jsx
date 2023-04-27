c9d400c03c842e9dd413d8c0e755aa1aa8cc69ce
Charleslogan <charlesokoro15@gmail.com>
2023-04-27 11:35:11 +0100

    first commit

---
+++ src/pages/Faqs/components/Accordion.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+export default function Accordion() {
+  return (
+    <div className="text-center flex">
+       Accordion
+       <div className="rotate-90"></div>
+    </div>
+  )
+}
